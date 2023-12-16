7ba012fb5a44553120c252954a71dcce9f8410a6
S P <shakthiman@gmail.com>
2023-12-16 00:39:44 +0000

    Modify Protein Cluster Lookup to lookup Cluster Id

--- protein_folding/protein_cluster_lookup.py
+++ protein_folding/protein_cluster_lookup.py
@@ -4,11 +4,12 @@ class ProteinClusterLookup(object):
 
   def __init__(self, clusters_by_entity_file_handler):
     self.entity_to_cluster_map = dict()
+    cluster = 0
     for r in clusters_by_entity_file_handler:
       entities = r.split()
-      num_entities = len(entities)
       for e in entities:
-        self.entity_to_cluster_map[e] = num_entities
+        self.entity_to_cluster_map[e] = cluster
+      cluster += 1
 
-  def NumEntitiesInProteinsCluster(self, pdb_id, entity_id):
-    return self.entity_to_cluster_map.get('{}_{}'.format(pdb_id, entity_id), 0)
+  def ProteinCluster(self, pdb_id, entity_id):
+    return self.entity_to_cluster_map.get('{}_{}'.format(pdb_id, entity_id), -1)
