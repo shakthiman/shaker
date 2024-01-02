48b9565e06e0f02cbac53447c505480e28ec4293
root <shakthima@gmail.com>
2024-01-02 03:40:43 +0000

    Use interleave to limit parallelism while reading TFRecordDataset

--- protein_folding/train_ds.py
+++ protein_folding/train_ds.py
@@ -8,8 +8,10 @@ def _IgnoreCondition(x):
   peptide_shapes = tf.map_fn(lambda y: tf.shape(y)[0], x['resname'], fn_output_signature=tf.int32)
   return tf.math.reduce_min(peptide_shapes)==0
 
-def _PrepareTFDataset(tf_dataset, num_parallel_calls):
-  return (tf_dataset.repeat()
+def _PrepareTFDataset(filenames, num_parallel_calls):
+  return (
+      tf.data.Dataset.from_tensor_slices(filenames).repeat()
+      .interleave(tf.data.TFRecordDataset, cycle_length=1)
       .map(training_example.ParseProteinOnlyExample,
           num_parallel_calls=num_parallel_calls)
       .filter(lambda x: not _IgnoreCondition(x)))
@@ -28,8 +30,7 @@ def GetTFExamples(project, bucket, blob_prefix, num_parallel_calls):
 
   raw_datasets = []
   for cluster, files in files_by_cluster.items():
-    raw_datasets.append(_PrepareTFDataset(
-      tf.data.TFRecordDataset(files), num_parallel_calls))
+    raw_datasets.append(_PrepareTFDataset(files, num_parallel_calls))
 
   return tf.data.Dataset.sample_from_datasets(raw_datasets)
 
@@ -47,7 +48,6 @@ def GetSmallTFExamples(project, bucket, blob_prefix, num_parallel_calls, size):
 
   raw_datasets = []
   for cluster, files in list(files_by_cluster.items())[:size]:
-    raw_datasets.append(_PrepareTFDataset(
-      tf.data.TFRecordDataset(files), num_parallel_calls))
+    raw_datasets.append(_PrepareTFDataset(files, num_parallel_calls))
 
   return tf.data.Dataset.sample_from_datasets(raw_datasets)
