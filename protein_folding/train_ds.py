a7da140710b6147eaab22f1e73d724b069977e55
root <shakthiman@gmail.com>
2024-01-02 02:50:12 +0000

    Switch from prefetching to parallel map calls

--- protein_folding/train_ds.py
+++ protein_folding/train_ds.py
@@ -8,13 +8,13 @@ def _IgnoreCondition(x):
   peptide_shapes = tf.map_fn(lambda y: tf.shape(y)[0], x['resname'], fn_output_signature=tf.int32)
   return tf.math.reduce_min(peptide_shapes)==0
 
-def _PrepareTFDataset(tf_dataset, prefetch_size):
+def _PrepareTFDataset(tf_dataset, num_parallel_calls):
   return (tf_dataset.repeat()
-      .map(training_example.ParseProteinOnlyExample)
-      .filter(lambda x: not _IgnoreCondition(x))
-      .prefetch(prefetch_size))
+      .map(training_example.ParseProteinOnlyExample,
+          num_parallel_calls=num_parallel_calls)
+      .filter(lambda x: not _IgnoreCondition(x)))
 
-def GetTFExamples(project, bucket, blob_prefix, prefetch_size):
+def GetTFExamples(project, bucket, blob_prefix, num_parallel_calls):
   client = storage.Client(project)
   blobs = client.list_blobs(bucket, prefix=blob_prefix)
   files_by_cluster = dict()
@@ -29,11 +29,11 @@ def GetTFExamples(project, bucket, blob_prefix, prefetch_size):
   raw_datasets = []
   for cluster, files in files_by_cluster.items():
     raw_datasets.append(_PrepareTFDataset(
-      tf.data.TFRecordDataset(files), prefetch_size))
+      tf.data.TFRecordDataset(files), num_parallel_calls))
 
   return tf.data.Dataset.sample_from_datasets(raw_datasets)
 
-def GetSmallTFExamples(project, bucket, blob_prefix, prefetch_size, size):
+def GetSmallTFExamples(project, bucket, blob_prefix, num_parallel_calls, size):
   client = storage.Client(project)
   blobs = client.list_blobs(bucket, prefix=blob_prefix)
   files_by_cluster = dict()
@@ -48,6 +48,6 @@ def GetSmallTFExamples(project, bucket, blob_prefix, prefetch_size, size):
   raw_datasets = []
   for cluster, files in list(files_by_cluster.items())[:size]:
     raw_datasets.append(_PrepareTFDataset(
-      tf.data.TFRecordDataset(files), prefetch_size))
+      tf.data.TFRecordDataset(files), num_parallel_calls))
 
   return tf.data.Dataset.sample_from_datasets(raw_datasets)
