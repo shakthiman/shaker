bc3727864d3b6360e7b458e49f99b078f2fbd75c
root <shakthiman@gmail.com>
2024-02-19 04:49:21 +0000

    Fix the padding parameter in the convolve layer

--- protein_folding/variational_model/models/sequence_latent_space_with_hierarchical_encoder.py
+++ protein_folding/variational_model/models/sequence_latent_space_with_hierarchical_encoder.py
@@ -180,7 +180,8 @@ def DecoderTransformLayer(num_blocks, num_heads, key_dim,
         [None, None, channel_size + _LATENT_EMBEDDING_SIZE])
     transformer_input = AttentionLayer(
         num_blocks, num_heads, key_dim, transformer_input, inputs_mask)
-    conv_inputs = tf.keras.layers.Conv1D(32, 100)(transformer_input)
+    conv_inputs = tf.keras.layers.Conv1D(32, 100, padding='same')(
+        transformer_input)
     x = FeedForwardLayer(num_dnn_layers, channel_size,
         tf.keras.layers.Dense(channel_size)(
             tf.keras.layers.concatenate([transformer_input, conv_inputs])))
