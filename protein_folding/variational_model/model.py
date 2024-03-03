Few Fixes to Rotation Model

protein_folding/variational_model/model.py
@@ -138,7 +138,7 @@ class VariationalModel(object):
 
   def _get_rotation_matrix(self, normalized_coordinates,
                            predicted_coordinates):
-    atom_mask = _XMask(training_data['normalized_coordinates'])
+    atom_mask = _XMask(normalized_coordinates)
     euler_rotation = self._rotation_model({
       'normalized_coordinates': normalized_coordinates,
       'atom_mask':atom_mask,
@@ -160,19 +160,26 @@ class VariationalModel(object):
     normalized_coordinates = training_data['normalized_coordinates']
     if self._rotation_model is not None:
       rot_matrix = self._get_rotation_matrix(
-          training_data['normalized_coordinates'], x.mean())
+          normalized_coordinates, x.mean())
+      normalized_coordinates_shape = tf.shape(normalized_coordinates)
       normalized_coordinates = rotation_matrix_3d.rotate(
           normalized_coordinates,
-          tf.expand_dims(tf.expand_dims(rot_matrix, 1), 1))
+          tf.broadcast_to(
+            tf.expand_dims(
+              tf.expand_dims(rot_matrix, 1), 1),
+            [normalized_coordinates_shape[0],
+             normalized_coordinates_shape[1],
+             normalized_coordinates_shape[2],
+             3,3]))
     logpx_z = tf.reduce_sum(
             tf.math.multiply(
-                x.log_prob(training_data['normalized_coordinates']),
+                x.log_prob(normalized_coordinates),
                 atom_mask), axis=[-1, -2])
     logpz = self._log_normal_pdf(z, 0., 0.)
     logqz_x = self._log_normal_pdf(z, mean, logvar)
     diff_mae = tf.math.reduce_sum(
             tf.math.multiply(
-                tf.math.abs(training_data['normalized_coordinates'] - x.mean()),
+                tf.math.abs(normalized_coordinates - x.mean()),
                 tf.expand_dims(atom_mask, -1)))/tf.math.reduce_sum(atom_mask)
 
     return LossInformation(

protein_folding/variational_model/models/sequence_latent_space_with_hierarchical_encoder.py
@@ -358,8 +358,9 @@ def CondModel(residue_lookup_size, atom_lookup_size):
 def RotationModel():
   normalized_coordinates = tf.keras.Input(shape=[None, None, 3],
                                           name='normalized_coordinates')
-  atom_mask = tf.keras.Input(shape=[None, None],
+  atom_mask_input = tf.keras.Input(shape=[None, None],
                              name='atom_mask')
+  atom_mask = tf.cast(atom_mask_input, tf.bool)
   predicted_coordinates = tf.keras.Input(shape=[None, None, 3],
                                          name='predicted_coordinates')
   input_features = tf.keras.layers.concatenate(
@@ -368,10 +369,10 @@ def RotationModel():
   straightened_mask = StraightenMultipeptideMask(atom_mask)
 
   prediction = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64))(
-      straightened_features, straightened_mask)
+      inputs=straightened_features, mask=straightened_mask)
   prediction = tf.keras.layers.Dense(64, activation='gelu')(prediction)
   prediction = tf.keras.layers.Dense(3)(prediction)
-  return tf.keras.Model(inputs=[normalized_coordinates, atom_mask, predicted_coordinates],
+  return tf.keras.Model(inputs=[normalized_coordinates, atom_mask_input, predicted_coordinates],
                         outputs=prediction)
 
 MODEL_FOR_TRAINING = lambda vocab: model.VariationalModel(