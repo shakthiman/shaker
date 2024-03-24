b37f90797f83b20023332520bdf8cf87dbec83a7
S P <shakthiman@gmail.com>
2024-03-24 03:03:47 +0000

    update the directory to which PDB examples are written

--- protein_folding/pdb_pipeline.py
+++ protein_folding/pdb_pipeline.py
@@ -23,8 +23,8 @@ def main():
 
     pdb_examplegen.DownloadTrainingExamples(
         pids,
-        "gs://unreplicated-training-data/pdb_training_examples_jan_27",
-        "gs://unreplicated-training-data/pdb_training_examples_summary/data_jan_27", runners.DataflowRunner(), options)
+        "gs://unreplicated-training-data/pdb_training_examples_mar_23",
+        "gs://unreplicated-training-data/pdb_training_examples_summary/data_mar_23", runners.DataflowRunner(), options)
 
 if __name__=="__main__":
     main()
