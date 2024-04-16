397cb41ff51070abde9ed85ba6e6d6a155f67886
root <shakthiman@gmail.com>
2024-04-16 00:05:43 +0000

    4th Round of Arishtam

--- assembly_training/no_scale_coordinates.py
+++ assembly_training/no_scale_coordinates.py
@@ -48,8 +48,8 @@ def main ():
     model=variational_model,
     optimizer=optimizer,
     save_frequency=1000,
-    write_target='gs://variational_shaker_models/assembly_based_model_prod_arishtam3',
-    tensorboard_target='gs://variational_shaker_models/tensorboard/assembly_based_model_prod_arishtam3',
+    write_target='gs://variational_shaker_models/assembly_based_model_prod_arishtam4',
+    tensorboard_target='gs://variational_shaker_models/tensorboard/assembly_based_model_prod_arishtam4',
     checkpoint_directory='gs://variational_shaker_models/checkpoints/assembly_based_model_prod_arishtam',
     strategy=strategy,
     beta_fn= lambda cpu_step: BetaAnneal(cpu_step))
