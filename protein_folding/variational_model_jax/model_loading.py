4db3ba4414496454573a379eaee1678cf32c30bb
Sailesh Prabhu <shakthiman@gmail.com>
2024-06-04 20:52:12 -0700

    Next Version of Adi

--- protein_folding/variational_model_jax/model_loading.py
+++ protein_folding/variational_model_jax/model_loading.py
@@ -26,3 +26,7 @@ def LoadModel(storage_client, bucket_name, blob_name, encoder_params, conditione
       _DownloadParams(bucket, blob_name + '/encoder', encoder_params),
       _DownloadParams(bucket, blob_name + '/conditioner', conditioner_params),
       _DownloadParams(bucket, blob_name + '/decoder', decoder_params))
+
+def LoadOptimizer(storage_client, bucket_name, blob_name, opt_state):
+  bucket = storage_client.bucket(bucket_name)
+  return _DownloadParams(bucket, blob_name + '/opt_state', opt_state)
