f6b29c36c581ea3ee6041d1809988791df537534
Sailesh Prabhu <shakthiman@gmail.com>
2024-06-21 21:09:50 -0700

    Next Mahat

--- assembly_training_jax/mahat.py
+++ assembly_training_jax/mahat.py
@@ -26,7 +26,7 @@ def main ():
                               cluster_shuffle_size=1000,
                               cluster_cycle_length=1000)
 
-  random_key = random.key(6)
+  random_key = random.key(7)
   vae = dropped_out_decoder.GetModel(
           batch_size=_BATCH_SIZE,
           input_length=_INPUT_SIZE,
@@ -48,12 +48,12 @@ def main ():
   vae_params = model_loading.LoadModelV2(
       storage_client=client,
       bucket_name='variational_shaker_models',
-      blob_name='assembly_based_jax_mahat_fixed_loss5/269000',
+      blob_name='assembly_based_jax_mahat_fixed_loss6/354000',
       vae_params=vae_params)
   opt_state = model_loading.LoadOptimizer(
       storage_client=client,
       bucket_name='variational_shaker_models',
-      blob_name='assembly_based_jax_mahat_fixed_loss5/269000',
+      blob_name='assembly_based_jax_mahat_fixed_loss6/354000',
       opt_state=opt_state)
 
   random_key, train_key = random.split(random_key, 2)
@@ -68,13 +68,13 @@ def main ():
     pdb_vocab=v,
     random_key=random_key,
     model_save_bucket='variational_shaker_models',
-    model_save_blob='assembly_based_jax_mahat_fixed_loss6',
-    tensorboard_target='gs://variational_shaker_models/tensorboard/assembly_based_jax_mahat_fixed_loss6',
+    model_save_blob='assembly_based_jax_mahat_fixed_loss7',
+    tensorboard_target='gs://variational_shaker_models/tensorboard/assembly_based_jax_mahat_fixed_loss7',
     vae=vae,
     optimizer=optimizer,
     vae_params=vae_params,
     opt_state=opt_state,
-    step=269000)
+    step=354000)
 
 if __name__ == "__main__":
   main()
