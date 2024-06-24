fd7397f6b5ad881ecc02e6a908a8074f77460ddb
Sailesh Prabhu <shakthiman@gmail.com>
2024-06-23 17:59:59 -0700

    next mahat

--- assembly_training_jax/mahat.py
+++ assembly_training_jax/mahat.py
@@ -1,3 +1,4 @@
+import jax
 from jax import random
 import tensorflow as tf
 
@@ -15,6 +16,7 @@ _BATCH_SIZE = 1
 _NUM_BLOCKS = 128
 
 def main ():
+  jax.profiler.start_server(9999)
   client = storage.Client()
   summary_blob = client.bucket("unreplicated-training-data").blob(
       "pdb_training_examples_summary/data_mar_26-00000-of-00001.avro")
@@ -26,7 +28,7 @@ def main ():
                               cluster_shuffle_size=1000,
                               cluster_cycle_length=1000)
 
-  random_key = random.key(9)
+  random_key = random.key(10)
   vae = dropped_out_decoder.GetModel(
           batch_size=_BATCH_SIZE,
           input_length=_INPUT_SIZE,
@@ -48,12 +50,12 @@ def main ():
   vae_params = model_loading.LoadModelV2(
       storage_client=client,
       bucket_name='variational_shaker_models',
-      blob_name='assembly_based_jax_mahat_fixed_loss8/500000',
+      blob_name='assembly_based_jax_mahat_fixed_loss9/510000',
       vae_params=vae_params)
   opt_state = model_loading.LoadOptimizer(
       storage_client=client,
       bucket_name='variational_shaker_models',
-      blob_name='assembly_based_jax_mahat_fixed_loss8/500000',
+      blob_name='assembly_based_jax_mahat_fixed_loss9/510000',
       opt_state=opt_state)
 
   random_key, train_key = random.split(random_key, 2)
@@ -68,13 +70,13 @@ def main ():
     pdb_vocab=v,
     random_key=random_key,
     model_save_bucket='variational_shaker_models',
-    model_save_blob='assembly_based_jax_mahat_fixed_loss9',
-    tensorboard_target='gs://variational_shaker_models/tensorboard/assembly_based_jax_mahat_fixed_loss9',
+    model_save_blob='assembly_based_jax_mahat_fixed_loss10',
+    tensorboard_target='gs://variational_shaker_models/tensorboard/assembly_based_jax_mahat_fixed_loss10',
     vae=vae,
     optimizer=optimizer,
     vae_params=vae_params,
     opt_state=opt_state,
-    step=500000)
+    step=510000)
 
 if __name__ == "__main__":
   main()
