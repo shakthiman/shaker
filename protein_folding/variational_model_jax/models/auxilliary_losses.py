00853a1d6c9aa226b7dba20c8f653f521f074bc3
Sailesh Prabhu <shakthiman@gmail.com>
2024-06-29 22:24:09 -0700

    Try just shifting the arrays

--- protein_folding/variational_model_jax/models/auxilliary_losses.py
+++ protein_folding/variational_model_jax/models/auxilliary_losses.py
@@ -14,49 +14,52 @@ ClashLoss = collections.namedtuple(
 def Clashes(mask, normalized_coordinates, training_data,
             loss_params, clash_params):
   def _SingleBatchLoss(single_batch_data):
-    def _SingleAtomLoss(atom_idx):
-      s_mask = single_batch_data['mask']
-      s_normalized_coordinates = single_batch_data['normalized_coordinates']
-      s_is_alpha_carbon = single_batch_data['is_alpha_carbon']
+    s_mask = single_batch_data['mask']
+    s_normalized_coordinates = single_batch_data['normalized_coordinates']
+    s_is_alpha_carbon = single_batch_data['is_alpha_carbon']
 
-      first_idx = min(loss_params.input_length-1,atom_idx+1)
-      last_idx = min(loss_params.input_length-1,
-                     atom_idx+clash_params.nearby_size+1)
-      neighborhood_mask = s_mask[first_idx:last_idx]
-      neighborhood_normalized_coordinates = s_normalized_coordinates[
-          first_idx:last_idx,:]
-      neighborhood_is_alpha_carbon = s_is_alpha_carbon[
-          first_idx:last_idx]
+    def _1DShift(arr, num):
+      shifted_array = jnp.pad(arr, [(0, num)])
+      return shifted_array[num:]
+    def _2DShift(arr, num):
+      shifted_array = jnp.pad(arr, [(0, num),
+                                    (0, 0)])
+      return shifted_array[num:,:]
 
-      my_coordinate = s_normalized_coordinates[atom_idx,:]
-      l2_distances = jnp.sum(jnp.square(
-        neighborhood_normalized_coordinates - my_coordinate),
+    neighborhood_mask = jnp.concatenate([
+        jnp.expand_dims(_1DShift(s_mask, num), axis=1)
+        for num in range(1,clash_params.nearby_size)], axis=1)
+
+    neighborhood_normalized_coordinates = jnp.concatenate([
+        jnp.expand_dims(_2DShift(s_normalized_coordinates, num), axis=1)
+        for num in range(1,clash_params.nearby_size)], axis=1)
+    neighborhood_is_alpha_carbon = jnp.concatenate([
+        jnp.expand_dims(_1DShift(s_is_alpha_carbon, num), axis=1)
+        for num in range(1,clash_params.nearby_size)], axis=1)
+
+    l2_distances = jnp.sum(jnp.square(
+        neighborhood_normalized_coordinates -
+        jnp.expand_dims(s_normalized_coordinates, axis=1)),
                              axis=-1)
-      is_hard_clash = jnp.less_equal(l2_distances, jnp.square(3.5))
-      is_soft_clash = jnp.less_equal(l2_distances, jnp.square(3.6))
-      num_hard_clashes= jnp.sum(jnp.where(
-          jnp.logical_and(
-            jnp.logical_and(s_mask[atom_idx],
-                            s_is_alpha_carbon[atom_idx]),
-            jnp.logical_and(neighborhood_mask,
-                           neighborhood_is_alpha_carbon)),
-          is_hard_clash, 0), axis=0)
-      num_soft_clashes= jnp.sum(jnp.where(
-          jnp.logical_and(
-            jnp.logical_and(
-              jnp.logical_and(s_mask[atom_idx],
-                              s_is_alpha_carbon[atom_idx]),
-              jnp.logical_and(neighborhood_mask,
-                              neighborhood_is_alpha_carbon)),
-              is_soft_clash),
-          jax.nn.sigmoid(20*(jnp.square(3.55)-l2_distances)), 0), axis=0)
-      return (num_hard_clashes, num_soft_clashes)
-    total_loss = (0, 0)
-    for i in range(loss_params.input_length-1):
-      sl = _SingleAtomLoss(i)
-      total_loss = (total_loss[0] + sl[0],
-                    total_loss[1] + sl[1])
-    return total_loss
+    is_hard_clash = jnp.less_equal(l2_distances, jnp.square(3.5))
+    is_soft_clash = jnp.less_equal(l2_distances, jnp.square(3.6))
+    num_hard_clashes= jnp.sum(jnp.where(
+      jnp.logical_and(
+        jnp.logical_and(s_mask,
+                        s_is_alpha_carbon),
+        jnp.logical_and(neighborhood_mask,
+                        neighborhood_is_alpha_carbon)),
+        is_hard_clash, 0), axis=[0,1])
+    num_soft_clashes= jnp.sum(jnp.where(
+      jnp.logical_and(
+        jnp.logical_and(
+          jnp.logical_and(s_mask,
+                          s_is_alpha_carbon),
+          jnp.logical_and(neighborhood_mask,
+                          neighborhood_is_alpha_carbon)),
+          is_soft_clash),
+      jax.nn.sigmoid(20*(jnp.square(3.55)-l2_distances)), 0), axis=[0,1])
+    return (num_hard_clashes, num_soft_clashes)
   num_hard_clashes, num_soft_clashes = jax.lax.map(
       _SingleBatchLoss, {'mask': mask,
                          'normalized_coordinates': normalized_coordinates,
