d6bf90f8b781dd4f7c1a95e4b7ccdc9b330301f4
Sailesh Prabhu <shakthiman@gmail.com>
2024-07-12 23:16:59 -0700

    Next Mahat

--- assembly_training_jax/mahat.py
+++ assembly_training_jax/mahat.py
@@ -28,7 +28,7 @@ def main ():
                               cluster_shuffle_size=1000,
                               cluster_cycle_length=1000)
 
-  random_key = random.key(11)
+  random_key = random.key(12)
   vae = dropped_out_decoder.GetModel(
           batch_size=_BATCH_SIZE,
           input_length=_INPUT_SIZE,
@@ -52,12 +52,12 @@ def main ():
   vae_params = model_loading.LoadModelV2(
       storage_client=client,
       bucket_name='variational_shaker_models',
-      blob_name='assembly_based_jax_mahat_consider_dihedral_loss12_fix_lower_dihedral_weight/750000',
+      blob_name='assembly_based_jax_mahat_consider_dihedral_loss13_fix_lower_dihedral_weight/840000',
       vae_params=vae_params)
   opt_state = model_loading.LoadOptimizer(
       storage_client=client,
       bucket_name='variational_shaker_models',
-      blob_name='assembly_based_jax_mahat_consider_dihedral_loss12_fix_lower_dihedral_weight/750000',
+      blob_name='assembly_based_jax_mahat_consider_dihedral_loss13_fix_lower_dihedral_weight/840000',
       opt_state=opt_state)
 
   random_key, train_key = random.split(random_key, 2)
@@ -78,7 +78,7 @@ def main ():
     optimizer=optimizer,
     vae_params=vae_params,
     opt_state=opt_state,
-    step=750000)
+    step=840000)
 
 if __name__ == "__main__":
   main()
