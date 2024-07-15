e4b3e490f96825fc9c244ebf9c894bc70cdf07cc
Sailesh Prabhu <shakthiman@gmail.com>
2024-07-14 21:52:28 -0700

    Next Mahat

--- assembly_training_jax/mahat.py
+++ assembly_training_jax/mahat.py
@@ -28,7 +28,7 @@ def main ():
                               cluster_shuffle_size=1000,
                               cluster_cycle_length=1000)
 
-  random_key = random.key(0)
+  random_key = random.key(1)
   vae = dropped_out_decoder.GetModel(
           batch_size=_BATCH_SIZE,
           input_length=_INPUT_SIZE,
@@ -49,16 +49,16 @@ def main ():
           batch_size=_BATCH_SIZE,
           input_length=_INPUT_SIZE)
   opt_state = optimizer.init(vae_params)
-  #vae_params = model_loading.LoadModelV2(
-  #    storage_client=client,
-  #    bucket_name='variational_shaker_models',
-  #    blob_name='assembly_based_jax_mahat_consider_dihedral_loss13_fix_lower_dihedral_weight/840000',
-  #    vae_params=vae_params)
-  #opt_state = model_loading.LoadOptimizer(
-  #    storage_client=client,
-  #    bucket_name='variational_shaker_models',
-  #    blob_name='assembly_based_jax_mahat_consider_dihedral_loss13_fix_lower_dihedral_weight/840000',
-  #    opt_state=opt_state)
+  vae_params = model_loading.LoadModelV2(
+      storage_client=client,
+      bucket_name='variational_shaker_models',
+      blob_name='assembly_based_jax_mahat_lower_clash_loss/95000',
+      vae_params=vae_params)
+  opt_state = model_loading.LoadOptimizer(
+      storage_client=client,
+      bucket_name='variational_shaker_models',
+      blob_name='assembly_based_jax_mahat_lower_clash_loss/95000',
+      opt_state=opt_state)
 
   random_key, train_key = random.split(random_key, 2)
   model_trainer_v2.Train(
@@ -78,7 +78,7 @@ def main ():
     optimizer=optimizer,
     vae_params=vae_params,
     opt_state=opt_state,
-    step=0)
+    step=95000)
 
 if __name__ == "__main__":
   main()
