6f7d1e2627fbfe999fdb6c156e6fc6ab31168a15
Sailesh Prabhu <shakthiman@gmail.com>
2024-07-20 22:10:54 -0700

    next Mahat

--- assembly_training_jax/mahat.py
+++ assembly_training_jax/mahat.py
@@ -28,7 +28,7 @@ def main ():
                               cluster_shuffle_size=1000,
                               cluster_cycle_length=1000)
 
-  random_key = random.key(7)
+  random_key = random.key(8)
   vae = dropped_out_decoder.GetModel(
           batch_size=_BATCH_SIZE,
           input_length=_INPUT_SIZE,
@@ -39,7 +39,7 @@ def main ():
           carbon=v.GetAtomNamesId(tf.constant('C')).numpy(),
           nitrogen=v.GetAtomNamesId(tf.constant('N')).numpy())
   optimizer = optax.chain(
-          optax.clip_by_global_norm(5e5),
+          optax.clip_by_global_norm(2e6),
           optax.adam(1e-3))
   optimizer = optax.MultiSteps(optimizer, every_k_schedule=32)
   random_key, model_init_key = random.split(random_key, 2)
@@ -52,13 +52,13 @@ def main ():
   vae_params = model_loading.LoadModelV2(
       storage_client=client,
       bucket_name='variational_shaker_models',
-      blob_name='assembly_based_jax_mahat_lower_clash_loss7/530000',
+      blob_name='assembly_based_jax_mahat_lower_clash_loss8/625000',
       vae_params=vae_params)
-  opt_state = model_loading.LoadOptimizer(
-      storage_client=client,
-      bucket_name='variational_shaker_models',
-      blob_name='assembly_based_jax_mahat_lower_clash_loss7/530000',
-      opt_state=opt_state)
+  #opt_state = model_loading.LoadOptimizer(
+  #    storage_client=client,
+  #    bucket_name='variational_shaker_models',
+  #    blob_name='assembly_based_jax_mahat_lower_clash_loss7/530000',
+  #    opt_state=opt_state)
 
   random_key, train_key = random.split(random_key, 2)
   model_trainer_v2.Train(
@@ -72,13 +72,13 @@ def main ():
     pdb_vocab=v,
     random_key=random_key,
     model_save_bucket='variational_shaker_models',
-    model_save_blob='assembly_based_jax_mahat_lower_clash_loss8',
-    tensorboard_target='gs://variational_shaker_models/tensorboard/assembly_based_jax_mahat_lower_clash_loss8',
+    model_save_blob='assembly_based_jax_mahat_lower_clash_loss9_reduce_gradient_clipping',
+    tensorboard_target='gs://variational_shaker_models/tensorboard/assembly_based_jax_mahat_lower_clash_loss9_reduce_gradient_clipping',
     vae=vae,
     optimizer=optimizer,
     vae_params=vae_params,
     opt_state=opt_state,
-    step=530000)
+    step=625000)
 
 if __name__ == "__main__":
   main()
