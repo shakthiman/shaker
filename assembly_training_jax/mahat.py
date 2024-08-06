c5d2b1b0dacf29f0c660b269c89967c0b93e9048
Sailesh Prabhu <shakthiman@gmail.com>
2024-08-05 22:09:41 -0700

    Next Mahat

--- assembly_training_jax/mahat.py
+++ assembly_training_jax/mahat.py
@@ -28,7 +28,7 @@ def main ():
                               cluster_shuffle_size=1000,
                               cluster_cycle_length=1000)
 
-  random_key = random.key(1)
+  random_key = random.key(2)
   vae = dropped_out_decoder.GetModel(
           batch_size=_BATCH_SIZE,
           input_length=_INPUT_SIZE,
@@ -52,12 +52,12 @@ def main ():
   vae_params = model_loading.LoadModelV2(
       storage_client=client,
       bucket_name='variational_shaker_models',
-      blob_name='assembly_based_jax_mahat_more_auxilliary_losses_retry/90000',
+      blob_name='assembly_based_jax_mahat_more_auxilliary_losses_retry2/125000',
       vae_params=vae_params)
   opt_state = model_loading.LoadOptimizer(
       storage_client=client,
       bucket_name='variational_shaker_models',
-      blob_name='assembly_based_jax_mahat_more_auxilliary_losses_retry/90000',
+      blob_name='assembly_based_jax_mahat_more_auxilliary_losses_retry2/125000',
       opt_state=opt_state)
 
   random_key, train_key = random.split(random_key, 2)
@@ -72,13 +72,13 @@ def main ():
     pdb_vocab=v,
     random_key=random_key,
     model_save_bucket='variational_shaker_models',
-    model_save_blob='assembly_based_jax_mahat_more_auxilliary_losses_retry2',
-    tensorboard_target='gs://variational_shaker_models/tensorboard/assembly_based_jax_mahat_more_auxilliary_losses_retry2',
+    model_save_blob='assembly_based_jax_mahat_more_auxilliary_losses_retry3',
+    tensorboard_target='gs://variational_shaker_models/tensorboard/assembly_based_jax_mahat_more_auxilliary_losses_retry3',
     vae=vae,
     optimizer=optimizer,
     vae_params=vae_params,
     opt_state=opt_state,
-    step=90000)
+    step=125000)
 
 if __name__ == "__main__":
   main()
